475c767d4c2e949b9422048f8ef5f8eabc6c5a7c
tomalley4 <tomalley@pps.net>
2018-04-30 00:37:43 -0700

    Remove user input size, set blue display color based on time of day

--- game_window.py
+++ game_window.py
@@ -1,13 +1,17 @@
 import pygame
 
-width = int(input("width? "))
-height = int(input("height? "))
-
-screen = pygame.display.set_mode((width, height))
+def daylight(time):
+    '''Takes an input of military time e.g. 800 = 8am, 1400 = 2pm
+    and uses that number to select a blue value that varies between
+    0 (midnight) and 255 (noon). Returns a RGB tuple.'''
+    blue_value = 255 * (1200 - abs(time - 1200)) // 1200
+    return (0, 0, blue_value)
+    
+screen = pygame.display.set_mode((640, 480))
 
 while True:
     event = pygame.event.poll()
     if event.type == pygame.QUIT:
         break
-    screen.fill((255, 255, 255))
+    screen.fill(daylight(500))
     pygame.display.flip()
