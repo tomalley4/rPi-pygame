bfa17587a087945e5d4eb38f5b2dc785e35e2411
jrheard <jrheard@cs.stanford.edu>
2018-04-30 17:45:58 -0700

    add rectangle code

--- game_window.py
+++ game_window.py
@@ -1,5 +1,7 @@
 import pygame
 
+pygame.init()
+
 def daylight(time):
     '''Takes an input of military time e.g. 800 = 8am, 1400 = 2pm
     and uses that number to select a blue value that varies between
@@ -9,9 +11,17 @@ def daylight(time):
     
 screen = pygame.display.set_mode((640, 480))
 
+def get_rect_color():
+    # TODO: instead of always returning red, make this function check the status
+    # of the rpi's switch and return blue if the switch has been pressed
+    return (255, 0, 0)
+
 while True:
     event = pygame.event.poll()
     if event.type == pygame.QUIT:
         break
     screen.fill(daylight(500))
+
+    pygame.draw.rect(screen, get_rect_color(), (50, 50, 100, 100))
+
     pygame.display.flip()
