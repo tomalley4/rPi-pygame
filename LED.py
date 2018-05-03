17e12b877df7fde029463e6610c9e8121e7d665b
tomalley <tomalley@pps.net>
2018-05-03 05:54:44 +0000

    Add button and start rpi code for game

--- LED.py
+++ LED.py
@@ -1,14 +1,20 @@
 from gpiozero import LED
 from time import sleep
 
-led = LED(18)
+#LED
+#.on()
+#.off()
+#.toggle()
+
+sunshine = LED(17)
 
 while True:
     print("LED on")
-    led.on()
+    sunshine.on()
     sleep(1)
     print("LED off")
-    led.off()
-    sleep(.5)
+    sunshine.off()
+    sleep(1.5)
+
 
 
